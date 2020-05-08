fe7fd921e8ee71a617317ef06eec1403791e89fc
aldodell <aldodell@gmail.com>
2020-05-08 17:04:45 -0400

    desde linux

--- compilar.sh
+++ compilar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clang++ -std=c++1z -stdlib=libc++ -lgmpxx -lgmp -DPRIME_TESTER_LIB=lib -DBIG_GEARS_FACTORIZER_LIB -DBIG_HALF_GEAR_FACTORIZER_LIB kdutils.cpp argumentsHandler.cpp maths_prime.cpp primeTester.cpp bigFactorObject.cpp bigGearsFactorizer.cpp bigHalfGearFactorizer.cpp aldo2.cpp -o aldo2.out --debug
+clang++ -std=c++1z -stdlib=libc++ -lgmpxx -lgmp -pthread -DPRIME_TESTER_LIB=lib -DBIG_GEARS_FACTORIZER_LIB -DBIG_HALF_GEAR_FACTORIZER_LIB kdutils.cpp argumentsHandler.cpp maths_prime.cpp primeTester.cpp bigFactorObject.cpp bigGearsFactorizer.cpp bigHalfGearFactorizer.cpp aldo2.cpp -o aldo2.out --debug -v
