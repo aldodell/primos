36f08aac1a975ec6f6f396a49febd15b5ceef832
aldodell <aldodell@gmail.com>
2020-10-20 18:31:55 -0400

    conjetura

--- tools/conjetura1.cpp
+++ tools/conjetura1.cpp
@@ -14,24 +14,27 @@ int main(int argc, char *argv[]) {
 
   vector<unsigned int> factors;
   string s;
-  unsigned int m, factor1, t;
+  unsigned int m, factor1, t, boundry;
   bool all4k1;
   mpz_class mersenne;
 
   // Valor inicial
   unsigned int p = atoi(argv[1]);
+  boundry = 10000000;
 
   while (true) {
 
-    // Evaluamos si el número es de la forma 4k+3
-    if (!is4k3(p)) {
-      // printf("%d isn't 4k+3 form\n", n);
-      goto nextN;
-    }
+    /*
+        // Evaluamos si el número es de la forma 4k+3
+        if (!is4k3(p)) {
+          // printf("%d isn't 4k+3 form\n", n);
+          goto nextN;
+        }
+        */
 
     // Cambiamos p por p-1/2
+    /*
     m = (p - 1) / 2;
-
     factors.clear();
     factorizer(m, factors);
 
@@ -39,41 +42,61 @@ int main(int argc, char *argv[]) {
     all4k1 = areAll4k1(factors);
 
     if (!all4k1) {
-      // printf("%d have not all factors with 4k+1 form.\n", n);
+      printf("%d have not all factors with 4k+1 form.\n", n);
       goto nextN;
     }
 
-    printf("found: %d(%d)\n", p, m);
-
-    // Mostramos los factores
-    /*
-    for (unsigned int q : factors) {
-      s = is4k3(q) ? "4k+3" : "4k+1";
-      printf("%d : %s\n", q, s.c_str());
-    }
+  printf("found: %d(%d)\n", p, m);
     */
 
     factor1 = (2 * p) + 1;
 
     if (!isPrime(factor1)) {
-      printf("%d factor isn't prime. \n", factor1);
+      // printf("2^%d-1; %d factor isn't prime. \n", p, factor1);
       goto nextN;
     }
+
+    // printf("2^%d-1; %d factor is prime. \n", p, factor1);
 
     mpz_ui_pow_ui(mersenne.get_mpz_t(), 2, p);
     mersenne--;
 
     if (mpz_divisible_ui_p(mersenne.get_mpz_t(), factor1) != 0) {
-      printf("2^%d-1 is divisible by %d\n", p, factor1);
+      // printf("2^%d-1 is divisible by %d\n", p, factor1);
+      // printf("%d\n", p);
+      printf("%d : %s\n==============\n", p, is4k1(p) ? "4k+1" : "4k+3");
+      m = (p - 1) / 2;
+      factors.clear();
+      factorizer(m, factors);
+      show4kXform(factors);
+      printf("\n");
+
+      // if (std::find(v.begin(), v.end(),value)!=v.end())
+      if (find(factors.begin(), factors.end(), 3) != factors.end()) {
+        printf("HAS 3!\n");
+        break;
+      }
+
+      if (is4k1(p)) {
+        printf("is 4k+1\n");
+        break;
+      }
+
       goto nextN;
     }
 
-    printf("Is prime? 2^%d-1 isn't divisible by %d\n", p, factor1);
-    break;
+    // printf("Is prime? 2^%d-1 isn't divisible by %d\n", p, factor1);
+    // break;
 
   nextN:
     p += 2;
     while (!isPrime(p))
       p += 2;
+
+    if (p > boundry) {
+      // printf("p=%d\n", p);
+      break;
+      boundry += 1000;
+    }
   }
 }
--- tools/insumo1.cpp
+++ tools/insumo1.cpp
@@ -1,4 +1,20 @@
 #include "insumo1.h"
+int main(int argc, char const *argv[]) {
 
+  vector<unsigned int> factors;
+  unsigned int t;
 
+  for (unsigned int p : MPE) {
+    factors.clear();
+    t = (p - 1) / 2;
 
+    if (t > 1) {
+      factorizer(t, factors);
+      printf("=== %d(%s) ===\n", p, is4k1(p) ? "4k+1" : "4k+3");
+      show4kXform(factors);
+      printf("\n");
+    }
+  }
+
+  return 0;
+}
